1a4f59cb7efc248f2d00ce3932b52d368e1de1dd
Max3796 <142794541+Max3796@users.noreply.github.com>
2023-10-13 09:43:02 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-JsEvents
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/Max3796/web-platform-JsEvents)
---
+++ script.js
@@ -0,0 +1,20 @@
+var user = document.getElementById("user")
+function printName(){
+  if (user){
+    user.innerHTML="Gurpreet"
+  }
+}
+window.onload= printName
+
+
+
+var btn = document.getElementById("btn-click")
+function btnClick(){
+  btn.style.backgroundColor= "yellow"
+}
+window.onclick = btnClick
+
+
+function makeSentence (){
+  
+}
